544491fb4e73e58fc550e2a52bfc586f64a61eca
Romel Santoral <ctechnology24@gmail.com>
2018-12-29 19:02:05 +0800

    set static file serving in settings

--- social_media/settings.py
+++ social_media/settings.py
@@ -127,4 +127,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.getenv(
+    'STATIC_ROOT',
+    os.path.join(BASE_DIR, 'public', 'static'))
+
+STATIC_URL = os.getenv('STATIC_URL', '/static/')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_URL = os.getenv('MEDIA_URL', '/media/')
+
+MEDIA_ROOT = os.getenv(
+    'MEDIA_ROOT',
+    os.path.join(BASE_DIR, 'public', 'media'))
