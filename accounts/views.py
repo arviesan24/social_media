d1e36f675f986da590004c75ab276abf5a3bff6e
Romel Santoral <ctechnology24@gmail.com>
2018-12-30 00:53:32 +0800

    Create `ProfileView`

--- accounts/views.py
+++ accounts/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import get_object_or_404
 
 from .models import User
 
+
+class ProfileView(DetailView):
+    """View for User Profile"""
+
+    template_name='accounts/profile.html'
+
+    def get_object(self):
+        return get_object_or_404(User, pk=self.request.user.id)
