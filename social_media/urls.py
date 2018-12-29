695641e5b7be79e53120f933b17e2e4c5e40b43e
Romel Santoral <ctechnology24@gmail.com>
2018-12-29 20:50:51 +0800

    add `accounts` urls

--- social_media/urls.py
+++ social_media/urls.py
@@ -17,11 +17,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('accounts/',
+        include(('accounts.urls', 'accounts'), namespace="accounts")),
     path('admin/', admin.site.urls),
 ]
 
