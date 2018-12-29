dd43070c43ca26de21c18826fde3772e28466fab
Romel Santoral <ctechnology24@gmail.com>
2018-12-30 00:06:35 +0800

    Fix `logout` url and line breaks

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,9 @@ from django.urls import reverse_lazy
 
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('login/',
+        LoginView.as_view(template_name='accounts/login.html'),name='login'),
     path('logout/',
-        LogoutView.as_view(next_page=reverse_lazy('login')), name='logout'),
+        LogoutView.as_view(next_page=reverse_lazy('accounts:login')),
+        name='logout'),
 ]
