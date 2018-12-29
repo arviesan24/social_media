189631781974b8cd5a560c9015274493be490303
Romel Santoral <ctechnology24@gmail.com>
2018-12-30 00:53:21 +0800

    Import `get_object_or_404

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .models import User
+
