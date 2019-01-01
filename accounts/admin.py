fce37224c3746245a1ab704f6ae35df37d360ab4
Romel Santoral <ctechnology24@gmail.com>
2019-01-01 21:24:08 +0800

    Import `UserAdminChangeForm`

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,6 +3,7 @@ from accounts.models import Relationship
 from accounts.models import Request
 from accounts.models import User
 
+from .forms import UserAdminChangeForm
 from .forms import UserAdminCreationForm
 
 
