29a5f2110a54b8d42205fbec34cff6be62210dab
Romel Santoral <ctechnology24@gmail.com>
2019-01-01 17:22:32 +0800

    Remove `User` model fields and change its base to `AbstractBaseUser`.

--- accounts/models.py
+++ accounts/models.py
@@ -3,35 +3,10 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
 
-class User(AbstractUser):
-    """Model Extending AbstractUser."""
+class User(AbstractBaseUser):
+    """Model Extending AbstractBaseUser."""
 
-    CHOICE_MALE = 'm'
-    CHOICE_FEMALE = 'f'
 
-    CHOICE_MEN = 'men'
-    CHOICE_WOMEN = 'women'
-    CHOICE_BOTH = 'both'
-
-    GENDER_CHOICES = (
-        (CHOICE_MALE, 'Male'),
-        (CHOICE_FEMALE, 'Female'),
-    )
-
-    PREFERENCE_CHOICES = (
-        (CHOICE_MEN, 'Men'),
-        (CHOICE_WOMEN, 'Women'),
-        (CHOICE_BOTH, 'Both'),
-    )
-
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-    preference = models.CharField(max_length=5, choices=PREFERENCE_CHOICES)
-    birth_date = models.DateField(blank=True, null=True)
-    phone_number = models.CharField(max_length=20, blank=True, null=True)
-    address = models.CharField(max_length=250, blank=True, null=True)
-    description = models.CharField(max_length=1000, blank=True, null=True)
-    datetime_created = models.DateTimeField(auto_now_add=True)
-    datetime_modified = models.DateTimeField(auto_now=True)
 
 
 class Relationship(models.Model):
