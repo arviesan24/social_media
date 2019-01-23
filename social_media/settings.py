b4a3f9c3bd6b0f0ac4ba76a6104893a4d2fe3c6e
Romel Santoral <ctechnology24@gmail.com>
2019-01-24 03:38:03 +0800

    Make the login urls in settings dynamic

--- social_media/settings.py
+++ social_media/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+from django.urls import reverse_lazy
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -80,8 +82,8 @@ WSGI_APPLICATION = 'social_media.wsgi.application'
 AUTH_USER_MODEL = 'accounts.User'
 
 
-LOGIN_URL = '/login/'
-LOGIN_REDIRECT_URL = '/accounts/profile/'
+LOGIN_URL = reverse_lazy('accounts:login')
+LOGIN_REDIRECT_URL = reverse_lazy('accounts:create-profile')
 
 
 # Database
