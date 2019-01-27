8d8f6f57540b5a4f9d405eedc9819453bb9c1eb2
Romel Santoral <ctechnology24@gmail.com>
2019-01-28 00:38:37 +0800

    Create `PostCreateView`

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .forms import PostForm
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    """View for Creating posts."""
+
+    # template_name = 'posts/form.html'
+    form_class = PostForm
+    success_url = reverse_lazy('accounts:profile')
+
+    def form_valid(self, form):
+        """Add `owner` in post."""
+        post = form.save(commit=False)
+        post.owner = self.request.user
+
+        return super().form_valid(form)
