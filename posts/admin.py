4e0cd1858faac505fdc57bb609c6f465a3ccaca9
Romel Santoral <ctechnology24@gmail.com>
2019-01-27 20:21:01 +0800

    Register `PostAdmin`

--- posts/admin.py
+++ posts/admin.py
@@ -2,3 +2,11 @@ from django_summernote.admin import SummernoteModelAdmin
 from django.contrib import admin
 
 from .models import Post
+
+
+class PostAdmin(SummernoteModelAdmin):
+    """Post model admin."""
+    summernote_fields = ('content',)
+
+
+admin.site.register(Post, PostAdmin)
