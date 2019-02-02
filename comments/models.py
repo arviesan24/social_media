fa2a26a004303cf164aab55ca214ee1b53362cc8
Romel Santoral <ctechnology24@gmail.com>
2019-02-03 00:43:40 +0800

    Fix `owner` field's `related_name` in `Comment` model

--- comments/models.py
+++ comments/models.py
@@ -5,8 +5,8 @@ class Comment(models.Model):
     """Model for comments."""
 
     owner = models.ForeignKey(
-        'accounts.User', related_name='posts',
-        related_query_name='post', on_delete=models.CASCADE)
+        'accounts.User', related_name='comments',
+        related_query_name='comment', on_delete=models.CASCADE)
     post = models.ForeignKey(
         'posts.Post', related_name='comments',
         related_query_name='comment', on_delete=models.CASCADE)
