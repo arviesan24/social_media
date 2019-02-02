b96f9dfcbddfec0bd715980bb6fd09c4dd36db6b
Romel Santoral <ctechnology24@gmail.com>
2019-02-02 23:54:55 +0800

    Update `content` field in `Comment` model

--- comments/models.py
+++ comments/models.py
@@ -10,7 +10,7 @@ class Comment(models.Model):
     post = models.ForeignKey(
         'posts.Post', related_name='comments',
         related_query_name='comment', on_delete=models.CASCADE)
-    content = models.TextField(blank=True, null=True)
+    content = models.TextField()
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
 
