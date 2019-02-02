37e9a53b90495c6809915cb4e7a2c785c49f04aa
Romel Santoral <ctechnology24@gmail.com>
2019-02-03 02:45:03 +0800

    Create `UserSerializer`

---
+++ accounts/serializers.py
@@ -0,0 +1,13 @@
+"""Serializers for Accounts app."""
+
+from rest_framework import serializers
+
+from .models import User
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'url', 'username', 'email', 'active',
+            'datetime_created', 'datetime_modified')
