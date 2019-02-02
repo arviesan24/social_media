5c146825eff1dec71ef5b6279132e1410d933b22
Romel Santoral <ctechnology24@gmail.com>
2019-02-03 02:48:53 +0800

    Create `PostViewSet`

---
+++ posts/viewsets.py
@@ -0,0 +1,10 @@
+"""Viewsets for Posts app."""
+
+from rest_framework import viewsets
+
+from . import serializers
+from . import models
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = models.Post.objects.all()
+    serializer_class = serializers.PostSerializer
