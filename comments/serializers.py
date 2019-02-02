33c2c7c85ecb595e8af94cbfb6fb2e685dd46fdb
Romel Santoral <ctechnology24@gmail.com>
2019-02-03 01:41:58 +0800

    Create `CommentSerializer`

---
+++ comments/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from .models import Comment
+
+
+class CommentSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Comment
+        fields = (
+            'url', 'owner', 'post', 'content', 'datetime_created',
+            'datetime_modified')
