6f31fd77fc102d66a2db6ab98f84acfc98038860
Romel Santoral <ctechnology24@gmail.com>
2019-02-03 02:46:44 +0800

    Create `UserViewSet`

---
+++ accounts/viewsets.py
@@ -0,0 +1,10 @@
+"""Viewsets for Accounts app."""
+
+from rest_framework import viewsets
+
+from . import serializers
+from . import models
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = models.User.objects.all()
+    serializer_class = serializers.UserSerializer
