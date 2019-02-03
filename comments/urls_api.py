1f0f8783cf42b8011d43f2582956b478c2d2ed40
Romel Santoral <ctechnology24@gmail.com>
2019-02-03 23:45:55 +0800

    Register router for `ReplyViewSet`

--- comments/urls_api.py
+++ comments/urls_api.py
@@ -1,9 +1,12 @@
 """Router config for Comments APIs"""
 
 from social_media.urls import router
+
 from .viewsets import CommentViewSet
+from .viewsets import ReplyViewSet
 
 
 router.register('comments', CommentViewSet)
+router.register('replies', ReplyViewSet)
 
 urlpatterns = []
