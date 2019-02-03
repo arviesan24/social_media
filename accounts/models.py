a2001010bf47b40178466c4a8741c1008781f7bd
Romel Santoral <ctechnology24@gmail.com>
2019-02-04 02:22:53 +0800

    Add `create_auth_token` signal

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,10 @@
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from rest_framework.authtoken.models import Token
 
 
 class User(AbstractUser):
@@ -96,3 +100,9 @@ class Request(models.Model):
     status = models.CharField(max_length=7, choices=STATUS_CHOICES)
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
