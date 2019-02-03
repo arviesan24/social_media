46f8395b79f242f753806450c99087a822c510f6
Romel Santoral <ctechnology24@gmail.com>
2019-02-04 00:28:34 +0800

    Import and load `load_dotenv` in settings

--- social_media/settings.py
+++ social_media/settings.py
@@ -15,6 +15,9 @@ import json
 
 from django.urls import reverse_lazy
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
