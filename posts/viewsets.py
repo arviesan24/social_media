8b96663607a662b254daa72ac195772eeeedb46d
Romel Santoral <ctechnology24@gmail.com>
2019-02-03 23:51:01 +0800

    Create `IsOwnerOrReadOnly` for posts viewset

--- posts/viewsets.py
+++ posts/viewsets.py
@@ -6,6 +6,22 @@ from rest_framework import permissions
 from . import serializers
 from . import models
 
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow post owner to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner of the snippet.
+        return obj.owner == request.user
+
+
 class PostViewSet(viewsets.ModelViewSet):
     queryset = models.Post.objects.all()
     serializer_class = serializers.PostSerializer
