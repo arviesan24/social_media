61a6ae771b5d51516b586a33bfd2e67574b7ba49
Romel Santoral <ctechnology24@gmail.com>
2019-02-09 20:24:36 +0800

    Add `content_type`, `object_id`, `content_object` remove `post` field

--- comments/models.py
+++ comments/models.py
@@ -9,9 +9,9 @@ class Comment(models.Model):
     owner = models.ForeignKey(
         'accounts.User', related_name='comments',
         related_query_name='comment', on_delete=models.CASCADE)
-    post = models.ForeignKey(
-        'posts.Post', related_name='comments',
-        related_query_name='comment', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
     content = models.TextField()
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
