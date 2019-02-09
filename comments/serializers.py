ce64c583de751b0ee322e446fae1b41bdd07a6cc
Romel Santoral <ctechnology24@gmail.com>
2019-02-09 15:54:29 +0800

    Delete `ReplySerializer`

--- comments/serializers.py
+++ comments/serializers.py
@@ -3,7 +3,6 @@
 from rest_framework import serializers
 
 from .models import Comment
-from .models import Reply
 
 
 class CommentSerializer(serializers.HyperlinkedModelSerializer):
@@ -13,14 +12,4 @@ class CommentSerializer(serializers.HyperlinkedModelSerializer):
         model = Comment
         fields = (
             'url', 'owner', 'post', 'content', 'datetime_created',
-            'datetime_modified')
-
-
-class ReplySerializer(serializers.HyperlinkedModelSerializer):
-    """Serializer for Reply Model."""
-
-    class Meta:
-        model = Reply
-        fields = (
-            'url', 'owner', 'comment', 'content', 'datetime_created',
             'datetime_modified')
