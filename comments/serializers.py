1e10340b7399614601c02bb58f99bedd84134d7f
Romel Santoral <ctechnology24@gmail.com>
2019-02-09 21:28:22 +0800

    Fix `content_object` serializer field

--- comments/serializers.py
+++ comments/serializers.py
@@ -4,9 +4,26 @@ from rest_framework import serializers
 
 from .models import Comment
 
+from posts.models import Post
+
+
+class ContentObjectRelatedField(serializers.RelatedField):
+    """A custom field to use for the `content_object` generic relationship."""
+
+    def to_representation(self, value):
+        """Serialize content objects to a class name representation."""
+        if isinstance(value, Comment):
+            return value.__class__.__name__
+        if isinstance(value, Post):
+            print(value)
+            return value.__class__.__name__
+        raise Exception('Unexpected type of tagged object')
+
 
 class CommentSerializer(serializers.HyperlinkedModelSerializer):
     """Serializer for Comment model."""
+
+    content_object = ContentObjectRelatedField(queryset=Comment.objects.all())
 
     class Meta:
         model = Comment
