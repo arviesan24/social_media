d34638e1ba783e3ad04d1acabddef26aa9a8ab0a
Romel Santoral <ctechnology24@gmail.com>
2019-02-11 02:21:25 +0800

    Create `PostCommentCreateView`

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+"""Views for `comments` app."""
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+
+from .forms import CommentForm
+
+
+class PostCommentCreateView(LoginRequiredMixin, CreateView):
+    """View for Comment creation."""
+
+    form_class = CommentForm
+    success_url = reverse_lazy('accounts:profile')
+
+    def get_form_kwargs(self):
+        """Pass `content_type_value` value to `CommentForm`."""
+        kwargs = super().get_form_kwargs()
+        kwargs['content_type_value'] = 'post'
+        return kwargs 
+
+    def form_valid(self, form):
+        """Save comment."""
+        comment = form.save(commit=False)
+        comment.object_id = form.cleaned_data.get('object_id')
+        comment.parent = None
+        comment.owner = self.request.user
+        return super().form_valid(form)
