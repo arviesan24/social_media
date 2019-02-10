571c3f8e7705ab3fc59bf3df0116e867082fe8b0
Romel Santoral <ctechnology24@gmail.com>
2019-02-11 03:18:21 +0800

    Create `create-reply` urls

--- comments/urls.py
+++ comments/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path(
         'create/', views.PostCommentCreateView.as_view(),
         name='create-comment'),
+    path(
+        'reply/create/', views.PostReplyCreateView.as_view(),
+        name='create-reply'),
 ]
