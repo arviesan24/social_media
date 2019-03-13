87fc972c3dde6c20d4f03a30c1526975c1f7e5b3
Romel Santoral <ctechnology24@gmail.com>
2019-03-14 01:28:24 +0800

    Create `IsProfileOwnerOrReadOnly`

--- accounts/viewsets.py
+++ accounts/viewsets.py
@@ -22,6 +22,21 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         return obj == request.user
 
 
+class IsProfileOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow profile owner to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner of the snippet.
+        return obj.user == request.user
+
+
 class UserViewSet(viewsets.ModelViewSet):
     """Viewset for UserSerializer"""
 
