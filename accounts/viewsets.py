bfa760eea01257c252d13d029899755d3d8841ed
Romel Santoral <ctechnology24@gmail.com>
2019-03-14 01:27:42 +0800

    Update `ProfileViewSet` `permission_classes`

--- accounts/viewsets.py
+++ accounts/viewsets.py
@@ -35,4 +35,5 @@ class ProfileViewSet(viewsets.ModelViewSet):
 
     queryset = models.Profile.objects.all()
     serializer_class = serializers.ProfileSerializer
-    permission_classes = (permissions.IsAuthenticated, IsOwnerOrReadOnly,)
+    permission_classes = (
+        permissions.IsAuthenticated, IsProfileOwnerOrReadOnly,)
