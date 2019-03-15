e34cca02fcdb1c57ca2f02df164c50fcf2189e12
Romel Santoral <ctechnology24@gmail.com>
2019-03-16 03:22:08 +0800

    Rename `CommentManager` `all` method to `comments`

--- comments/models.py
+++ comments/models.py
@@ -6,8 +6,8 @@ from django.db import models
 class CommentManager(models.Manager):
     """Model manager for `Comment` model."""
 
-    def all(self):
-        """Return results of instance with no parent (not a reply)."""
+    def comments(self):
+        """Return results of instance with no parent."""
         qs = super().filter(parent=None)
         return qs
 
