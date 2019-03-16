618258755c672e255c5b7d83239ff454d59de940
Romel Santoral <ctechnology24@gmail.com>
2019-03-16 23:45:03 +0800

    Add comments in settings

--- social_media/settings.py
+++ social_media/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = json.loads(os.getenv('DEBUG', 'false'))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] # don't forget to update `USE_HTTPS` and `CURRENT_DOMAIN` too for APIs
 
 
 # Application definition
