c0b0d563a41c23709d3a1e644a3a54f4e54e3240
Romel Santoral <ctechnology24@gmail.com>
2019-04-06 23:23:38 +0800

    Update `ImageViewSet` and `AlbumViewSet` `permission_classes`

--- images/viewsets.py
+++ images/viewsets.py
@@ -8,19 +8,34 @@ from . import serializers
 from . import models
 
 
-# class IsOwnerOrReadOnly(permissions.BasePermission):
-#     """
-#     Custom permission to only allow object owner to edit it.
-#     """
+class IsAlbumOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow object owner to edit it.
+    """
 
-#     def has_object_permission(self, request, view, obj):
-#         # Read permissions are allowed to any request,
-#         # so we'll always allow GET, HEAD or OPTIONS requests.
-#         if request.method in permissions.SAFE_METHODS:
-#             return True
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
 
-#         # Write permissions are only allowed to the owner of the snippet.
-#         return obj.owner == request.user
+        # Write permissions are only allowed to the owner of the snippet.
+        return obj.owner == request.user
+
+
+class IsImageOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow album owner to edit image.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner of the snippet.
+        return obj.album.owner == request.user
 
 
 # class ImageFilterSet(django_filters.FilterSet):
@@ -59,7 +74,7 @@ class ImageViewSet(viewsets.ModelViewSet):
 
     queryset = models.Image.objects.all()
     serializer_class = serializers.ImageSerializer
-    # permission_classes = (permissions.IsAuthenticated, IsOwnerOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticated, IsImageOwnerOrReadOnly,)
     filter_backends = (django_filters.DjangoFilterBackend,)
     # filterset_class = CommentFilterSet
 
@@ -69,6 +84,6 @@ class AlbumViewSet(viewsets.ModelViewSet):
 
     queryset = models.Album.objects.all()
     serializer_class = serializers.AlbumSerializer
-    # permission_classes = (permissions.IsAuthenticated, IsOwnerOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticated, IsAlbumOwnerOrReadOnly,)
     filter_backends = (django_filters.DjangoFilterBackend,)
     # filterset_class = CommentFilterSet
