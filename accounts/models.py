7ea47566355cb6a32a1939fac4f5b5bbd1440ea9
Romel Santoral <ctechnology24@gmail.com>
2019-04-30 21:22:43 +0800

    Make `Request` model `status` field unique

--- accounts/models.py
+++ accounts/models.py
@@ -89,7 +89,7 @@ class Relationship(models.Model):
 class Request(models.Model):
     """Model for Request."""
 
-    status = models.CharField(max_length=50)
+    status = models.CharField(max_length=50, unique=True)
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
 
