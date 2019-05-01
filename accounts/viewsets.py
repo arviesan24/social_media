505754d94f6b9a964172fc63538deac00f9b9077
Romel Santoral <ctechnology24@gmail.com>
2019-05-01 11:53:16 +0800

    Import `Q`

--- accounts/viewsets.py
+++ accounts/viewsets.py
@@ -1,5 +1,6 @@
 """Viewsets for Accounts app."""
 
+from django.db.models import Q
 from django_filters import rest_framework as django_filters
 from rest_framework import viewsets
 from rest_framework import permissions
