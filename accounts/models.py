359be8c9340ec530d9c3c9da7a8646d041b02522
Romel Santoral <ctechnology24@gmail.com>
2019-05-02 14:29:26 +0800

    Import `slugify`

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
+from django.utils.text import slugify
 
 from rest_framework.authtoken.models import Token
 
