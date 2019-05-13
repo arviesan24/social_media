9e5c8f391141d5fe612fe8b7da4c603f8215b9f5
Romel Santoral <ctechnology24@gmail.com>
2019-05-13 19:50:00 +0800

    Set `REST_FRAMEWORK.DEFAULT_PAGINATION_CLASS` in settings

--- social_media/settings.py
+++ social_media/settings.py
@@ -130,6 +130,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
 }
 
 
