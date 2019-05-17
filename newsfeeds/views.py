da985919fb1e05e71c5b194581123bfb895f76e3
Romel Santoral <ctechnology24@gmail.com>
2019-05-18 00:00:23 +0800

    Add `prefetch_related` to `usr_stream` function call

--- newsfeeds/views.py
+++ newsfeeds/views.py
@@ -16,7 +16,8 @@ class NewsFeedTemplateView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):
         """Return context data to the template."""
         context = super().get_context_data(**kwargs)
-        usr_stream = user_stream(self.request.user).prefetch_related('actor')
+        usr_stream = user_stream(
+            self.request.user).prefetch_related('actor', 'action_object')
         # add `user_stream` to context variable.
         context['feeds'] = usr_stream
 
