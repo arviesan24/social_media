5e8954a35a1295b8944be749b31a323016711ad9
Romel Santoral <ctechnology24@gmail.com>
2019-05-27 15:35:59 +0800

    Remove unused imports

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,6 @@
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.db.models.signals import post_save
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
-from django.utils.text import slugify
-
-from rest_framework.authtoken.models import Token
 
 
 class User(AbstractUser):
